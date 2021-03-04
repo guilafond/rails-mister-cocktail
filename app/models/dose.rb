aca0a976c62816dea2861616c546decb22b8a9f1
Guillaume Lafond <g.lafond@gmail.com>
2021-03-04 12:20:41 +0100

    all models OK + seedswith JSON api

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -3,4 +3,8 @@ class Dose < ApplicationRecord
   belongs_to :ingredient
 
   validates :description, presence: true
+  validates_uniqueness_of :cocktail_id, scope: :ingredient_id
+
+  # validates_uniqueness_of :dose_id, scope: [:cocktail_id, :ingredient_id]
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+require 'json'
+require 'open-uri'
+
+
+
+filepath = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+
+ingredients = JSON.parse(open(filepath).read)["drinks"]
+# should give a HASH type object { drinks: [ {"strIngredient1":"Light rum"} ] }
+
+Ingredient.destroy_all
+
+ingredients.each do |ingredient|
+  Ingredient.create(name: ingredient["strIngredient1"])
+end
+
+# ==> "Light Rum"
+
+# Ingredient.create(name: "lemon")
+# Ingredient.create(name: "ice")
+# Ingredient.create(name: "mint leaves")
